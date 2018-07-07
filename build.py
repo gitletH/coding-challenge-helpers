3a8d29471dac6f88ed79e71a5683e0d8edfbddfd
Tianjiao Huang <maitian519@gmail.com>
2018-07-06 22:38:00 -0700

    add build file

---
+++ build.py
@@ -0,0 +1,18 @@
+import glob
+import re
+import os
+
+srcDir = 'src/'
+outDir = 'out/'
+
+if not os.path.exists(outDir):
+    os.makedirs(outDir)
+
+txtfiles = []
+for file in glob.glob("%s*.py" % srcDir):
+    with open(file, 'r') as fin:
+        with open(file.replace(srcDir, outDir), 'w+') as fout:
+            for line in fin:
+                if not re.match('(?:^\s*$)|(?:^#.*$)', line):
+                    fout.write(line)
+                    
